256482f57810a157660a9847f22afbc32b246fb7
Jeroen van Beek <losbeekos@gmail.com>
2023-06-03 14:52:53 +0200

    feat: add angular/pwa

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 import { APP_ROUTES } from './app/app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
+import { isDevMode } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient(), provideRouter(APP_ROUTES)],
+  providers: [provideHttpClient(), provideRouter(APP_ROUTES), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })],
 });
